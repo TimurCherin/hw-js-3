b5dd12ecd0bdcdcd73622c31ac3be22438810bd5
TimurCherin <109847059+TimurCherin@users.noreply.github.com>
2023-04-06 17:58:13 +0300

    hw-3

---
+++ js/index.js
@@ -0,0 +1,20 @@
+                                                // ex 1
+const k1 = "5px";
+const k2 = "12djd334";
+const k3 = "12.45asdwe12";
+const k4 = "qwqweeewq";
+console.log(Number.parseInt(k1));
+console.log(Number.parseInt(k2));
+console.log(Number.parseInt(k3));
+console.log(Number.parseInt(k4));
+                                                // ex 2
+console.log(Number.parseFloat(k1));
+console.log(Number.parseFloat(k2));
+console.log(Number.parseFloat(k3));
+console.log(Number.parseFloat(k4));
+                                                // ex 3
+console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18));
+                                                // ex 4
+const max = 19;
+const min = 3;
+console.log(Math.random() * (19 - 3) + 1);
